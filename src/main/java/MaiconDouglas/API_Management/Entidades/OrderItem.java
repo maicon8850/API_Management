6c517e25e9c27f55c913285b9a2a7da8d82bf234
maicon-douglas <maicon.douglas3527@gmail.com>
2025-01-18 12:00:16 -0300

    feat: adicionados métodos para cálculo de subtotal e total de pedidos
    - Implementado o método `getSubTotal()` na classe `OrderItem` para calcular o valor total de um item baseado na quantidade e no preço.
    - Adicionado o método `getTotal()` na classe `Order` para calcular o valor total de um pedido somando os subtotais de todos os itens.
    - Atualizada a lógica de associações entre `Order` e `OrderItem`, garantindo consistência nos cálculos e na estrutura do banco de dados.
    - Anotação `@JsonIgnore` adicionada no método `getOrder()` de `OrderItem` para evitar loops na serialização JSON.

--- src/main/java/MaiconDouglas/API_Management/Entidades/OrderItem.java
+++ src/main/java/MaiconDouglas/API_Management/Entidades/OrderItem.java
@@ -49,6 +49,9 @@ public class OrderItem implements Serializable {
     public void setPrice(Double price) {
         this.price = price;
     }
+    public Double getSubTotal() {
+        return price * quantity;
+    }
     @Override
     public int hashCode() {
         final int prime = 31;
